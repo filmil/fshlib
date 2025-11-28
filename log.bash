5f67a2367c650704865089bd970141fcf66ba4f5
Filip Filmar <filmil@gmail.com>
2025-11-28 02:33:28 +0000

    feat: initial commit

---
+++ log.bash
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+function log::info() {
+    local _green="\033[32m"
+    local _normal="\033[0m"
+    echo -e "${_green}INFO:${_normal} ${@}"
+}
+
+function log::warn() {
+    local _green="\033[33m"
+    local _normal="\033[0m"
+    echo -e "${_green}WARN:${_normal} ${@}"
+}
+
+function log::error() {
+    local _green="\033[31m"
+    local _normal="\033[0m"
+    echo -e "${_green}ERROR:${_normal} ${@}"
+}
+
+function log::prefix() {
+  local _prefix="${1:-}"
+  sed "s/^/$_prefix/"
+}
+
